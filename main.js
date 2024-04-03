570d508d089b63f1273bf10bd04943e1be1448bd
82Sam <53128739+Ivyson@users.noreply.github.com>
2024-04-03 11:35:53 +0000

    !

---
+++ main.js
@@ -0,0 +1,9 @@
+function multiplyMat(mat1, mat2)
+{
+    if(mat1.length == mat2.length)
+    {
+        //The sizes might just be the same
+        let result, sum = 0;
+        for(let i =0)
+    }
+}
